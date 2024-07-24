23a8548f204e47b9d92997fa8a65fb4fcb63a7b7
m_shadkam <m_shadkam@ISC.CO.IR>
2024-07-24 10:30:44 +0330

    add route configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('@nx-learning/products').then((m) => m.ProductsComponent),
+  },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('@nx-learning/orders').then((m) => m.OrdersComponent),
+  },
+];
+
